2082c2b40d7d84de6e4c0af55acec242a094f715
Jesús Domingo Chisvert <jesuggio@MacBook-Air-de-Jesus.local>
2024-06-03 20:49:47 +0200

    create 2 .csv

--- api.py
+++ api.py
@@ -1,9 +1,14 @@
 import requests
 import json
 
-url = " https://api.covidtracking.com/v1/us/daily.json"
-response = requests.get(url)
-data = response.json()
+url = "https://api.covidtracking.com/v1/us/20200501.json"
 
-with open ('daily.json', 'w') as file:
-    json.dump(data, file)
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+    with open('data_covid.json', 'w') as file:
+        json.dump(data, file)
+    print("Datos guardados exitosamente.")
+else:
+    print("Error en la solicitud: ", response.status_code)
